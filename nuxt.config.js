77023135b5b639b64ab099eee6f302e0ce5ddfc0
kimura.m <k422756+git@gmail.com>
2020-05-21 23:23:47 +0900

    metaタグ修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,19 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.SITE_DESCRIPTION || ''
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: process.env.SITE_NAME
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://k422756.com' },
+      { hid: 'og:title', property: 'og:title', content: process.env.SITE_NAME },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: process.env.SITE_DESCRIPTION
       }
     ],
     link: [
